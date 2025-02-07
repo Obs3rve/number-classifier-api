f9895226cd8e1673d74bb06186d61b6ab78b080a
Obs3rve <Otumbalaurel@gmail.com>
2025-02-07 04:48:30 +0000

    updated flask port to 8000

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def classify_number():
         return jsonify({"number": "invalid", "error": True}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
 
